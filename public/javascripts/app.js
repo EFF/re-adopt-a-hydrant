2a74e18b08c57a3830f5c2b6750ba4609fd8059c
Francois-Xavier <frxpoulin@gmail.com>
2013-04-23 00:28:23 +0200

    add the map to the dom

---
+++ public/javascripts/app.js
@@ -0,0 +1,21 @@
+var app = angular.module('reAdoptAHydrant', []);
+
+app.controller('Map', function($scope){
+});
+
+app.directive('map', function () {
+    return {
+        restrict: 'E',
+        template: '<div></div>',
+        link: function ($scope, element, attrs) {
+            var options = {
+                center: new google.maps.LatLng(46.813953,-71.207972),
+                replace: true,
+                zoom: 12,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            var map = new google.maps.Map(document.getElementById("map-canvas"), options);
+        }
+    } 
+});
