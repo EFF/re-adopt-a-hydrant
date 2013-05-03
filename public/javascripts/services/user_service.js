a2af4060a6190973b3aa201a5514cbf3b96d11d1
Francois-Xavier <frxpoulin@gmail.com>
2013-05-03 18:28:09 +0200

    user info style and add marker images

--- public/javascripts/services/user_service.js
+++ public/javascripts/services/user_service.js
@@ -11,6 +11,7 @@ reAdoptAHydrant.services.UserService = function($http){
             $http(config)
                 .success(function(data, status, headers, config){
                     callback(null, data);
+                    console.log(data);
                 })
                 .error(function(data, status, headers, config){
                     callback(null, false)
