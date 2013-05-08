02736143e38a5ff43b5b509185ee3bb06af7533e
jack_dbernier <jdesjardinsbernier@gmail.com>
2013-05-08 15:34:27 +0200

    marker drop

--- public/javascripts/controllers/map_controller.js
+++ public/javascripts/controllers/map_controller.js
@@ -2,11 +2,12 @@ goog.provide('reAdoptAHydrant.controllers.Map');
 
 reAdoptAHydrant.controllers.Map = function($scope, HydrantService){
     var isPaused = false;
+    var hydrants = {};
 
     var options = {
         center: new google.maps.LatLng(46.813953,-71.207972),
         replace: true,
-        zoom: 12,
+        zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
@@ -21,12 +22,43 @@ reAdoptAHydrant.controllers.Map = function($scope, HydrantService){
                         console.log(err);
                     }
                     else{
-                        console.log(data);
+                        var count = 0;
+                        for (var i = 0; i < data.length; i++){
+                            var hydrant = data[i];
+                            if(!hydrants[hydrant._id]){
+                                count++;
+                                hydrants[hydrant._id] = true;
+                                setTimeout(createMarker.bind(createMarker, hydrant._source.location.lat,hydrant._source.location.lon), count * 200);
+                            }
+                        }
                     }
                 });
             }, 1000);
         }
     });
+
+    var createMarker = function(lat, lon){
+        var icon = {
+            anchor: new google.maps.Point(16,39),
+            size: new google.maps.Size(33,39),
+            url: '/images/normalHydrantMarker.png'
+        };
+        var shadow = {
+            anchor: new google.maps.Point(6,24),
+            size: new google.maps.Size(40,24),
+            url: '/images/markerShadow.png'
+        };
+
+        var markerOptions = {
+            map: map,
+            animation: google.maps.Animation.DROP,
+            position: new google.maps.LatLng(lat, lon),
+            icon: icon,
+            shadow: shadow
+        };
+
+        return new google.maps.Marker(markerOptions);
+    };
 
     var handleUserPosition = function(position){
         setMapCenter(position.coords.latitude,position.coords.longitude);
