1c956e1bd0a89c4b40e83213d7d749aea28679d8
jack_dbernier <jdesjardinsbernier@gmail.com>
2013-05-15 13:20:27 +0200

    move unit tests to frontend-unit .. we will have backend-unit tests eventually

--- tests/config/karma.conf.unit.js
+++ tests/config/karma.conf.unit.js
@@ -5,7 +5,7 @@ files = [
   {pattern: '../unit/no_deps_bullshit.js', included: true, watched: false, served: true},
   {pattern: '../../vendors/closure-library/closure/goog/base.js', included: true, watched: false, served: true},
   {pattern: '../../vendors/closure-library/closure/**/*.js', included: false, watched: false, served: true},
-  {pattern: '../unit/deps.js', include: true, watched: true, served: true},
+  {pattern: '../frontend-unit/deps.js', include: true, watched: true, served: true},
   {pattern: '../../public/javascripts/**/*.js', include: false, watched: true, served: true},
   {pattern: '../frontend-unit/*.test.*'},
   {pattern: '../../node_modules/chai/chai.js'}
